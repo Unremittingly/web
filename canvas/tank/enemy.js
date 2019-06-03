0f9fb978810b935d0b73fbb6531b0904fe2b68cc
TS.CTU.zhangqiang <13608164790zq93>
2019-06-03 16:46:29 +0800

    update

--- canvas/tank/enemy.js
+++ canvas/tank/enemy.js
@@ -1,10 +1,18 @@
+
+const enemyType = {
+    normal:1,
+    fast:2,
+    big:3,
+    bigB:4
+};
 //敌机
 class Enemy extends Base{
     constructor(ctx) {
         super();
         console.log('enemy');
         this.ctx = ctx;
-        this.pos = {x: 0, y: 0}
+        this.pos = {x: 0, y: 0};
+        this.isRed = false;//是否标红可以出道具
     }
 
 }
